3071a4820c5173d4187fe554f5cbda109a40c604
Keanu <keanutimmermans8@gmail.com>
2020-06-01 14:45:00 +0200

    Did multiple things.
    - Added command handler.
    - Added hello command.
    - Added utility commands.
    - Added lockfile.

--- src/Structures/BotClient.js
+++ src/Structures/BotClient.js
@@ -1,4 +1,5 @@
-const { Client } = require('discord.js');
+const { Client, Collection } = require('discord.js');
+const Util = require('./Util.js');
 
 module.exports = class BotClient extends Client {
 
@@ -7,6 +8,12 @@ module.exports = class BotClient extends Client {
 			disableMentions: 'everyone'
 		});
 		this.validate(options);
+
+		this.commands = new Collection();
+
+		this.aliases = new Collection();
+
+		this.utils = new Util(this);
 
 		this.once('ready', () => {
 			console.log(`Logged in as ${this.user.username}.`);
@@ -26,8 +33,9 @@ module.exports = class BotClient extends Client {
 			// eslint-disable-next-line no-unused-vars
 			const [cmd, ...args] = message.content.slice(prefix.length).trim().split(/ +/g);
 
-			if (cmd.toLowerCase() === 'hello') {
-				message.channel.send('Hello!');
+			const command = this.commands.get(cmd.toLowerCase()) || this.commands.get(this.aliases.get(cmd.toLowerCase()));
+			if (command) {
+				command.run(message, args);
 			}
 		});
 	}
@@ -44,6 +52,7 @@ module.exports = class BotClient extends Client {
 	}
 
 	async start(token = this.token) {
+		this.utils.loadCommands();
 		super.login(token);
 	}
 
