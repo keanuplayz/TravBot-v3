44c6dfab1b4c5cf52e19c1cf3957dd8efa162057
Keanu <keanutimmermans8@gmail.com>
2020-07-01 10:59:42 +0200

    Fixed ESLint errors.

--- src/Commands/Hello.js
+++ src/Commands/Hello.js
@@ -1,13 +1,15 @@
-const Command = require("./../Structures/Command.js");
+const Command = require('./../Structures/Command.js');
 
 module.exports = class extends Command {
-  constructor(...args) {
-    super(...args, {
-      aliases: ["hallo"]
-    })
-  }
 
-  async run(message, args) {
-    message.channel.send("Hello");
-  }
-}
+	constructor(...args) {
+		super(...args, {
+			aliases: ['hallo']
+		});
+	}
+
+	async run(message, args) {
+		message.channel.send('Hello');
+	}
+
+};
--- src/Commands/Utilities/Uptime.js
+++ src/Commands/Utilities/Uptime.js
@@ -1,10 +1,10 @@
-const Command = require("../../Structures/Command.js");
-const ms = require("ms");
+const Command = require('../../Structures/Command.js');
+const ms = require('ms');
 
 module.exports = class extends Command {
 
-  async run(message) {
-    message.channel.send(`My uptime is \`${ms(this.client.uptime, {long: true})}\``);
-  }
+	async run(message) {
+		message.channel.send(`My uptime is \`${ms(this.client.uptime, { long: true })}\``);
+	}
 
-}
+};
