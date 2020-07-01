5645d856cffa23bf4d50f7b89ed39f1557023193
Keanu <keanutimmermans8@gmail.com>
2020-07-01 14:38:07 +0200

    Added removeDuplicates and capitalise functions.

--- src/Structures/Util.js
+++ src/Structures/Util.js
@@ -27,12 +27,22 @@ module.exports = class Util {
 		}
 		return arr;
 	}
+
 	formatBytes(bytes) {
 		if (bytes === 0) return '0 Bytes';
 		const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
 		const i = Math.floor(Math.log(bytes) / Math.log(1024));
 		return `${parseFloat((bytes / Math.pow(1024, i)).toFixed(2))} ${sizes[i]}`;
 	}
+
+	removeDuplicates(arr) {
+		return [...new Set(arr)];
+	}
+
+	capitalise(string) {
+		return string.split(' ').map(str => str.slice(0, 1).toUpperCase() + str.slice(1)).join(' ');
+	}
+
 	async loadCommands() {
 		return glob(`${this.directory}commands/**/*.js`).then(commands => {
 			for (const commandFile of commands) {
