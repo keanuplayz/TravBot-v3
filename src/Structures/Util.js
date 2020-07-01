3033b791f576f0137f3feca25305abf8441c5be8
Keanu <keanutimmermans8@gmail.com>
2020-07-01 14:26:58 +0200

    Added userinfo and botinfo command.

--- src/Structures/Util.js
+++ src/Structures/Util.js
@@ -19,6 +19,20 @@ module.exports = class Util {
 		return `${path.dirname(require.main.filename)}${path.sep}`;
 	}
 
+	trimArray(arr, maxLen = 10) {
+		if (arr.length > maxLen) {
+			const len = arr.length - maxLen;
+			arr = arr.slice(0, maxLen);
+			arr.push(`${len} more...`);
+		}
+		return arr;
+	}
+	formatBytes(bytes) {
+		if (bytes === 0) return '0 Bytes';
+		const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+		const i = Math.floor(Math.log(bytes) / Math.log(1024));
+		return `${parseFloat((bytes / Math.pow(1024, i)).toFixed(2))} ${sizes[i]}`;
+	}
 	async loadCommands() {
 		return glob(`${this.directory}commands/**/*.js`).then(commands => {
 			for (const commandFile of commands) {
@@ -37,14 +51,4 @@ module.exports = class Util {
 			}
 		});
 	}
-
-	trimArray(arr, maxLen = 10) {
-		if (arr.length > maxLen) {
-			const len = arr.length - maxLen;
-			arr = arr.slice(0, maxLen);
-			arr.push(`${len} more...`);
-		}
-		return arr;
-	}
-
 };
