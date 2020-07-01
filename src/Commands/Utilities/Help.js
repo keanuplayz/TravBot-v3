58787c907cb9368b56d02854726a8c246972ca43
Keanu <keanutimmermans8@gmail.com>
2020-07-01 14:35:08 +0200

    Added a simple help command.
    Currently does nothing yet.

---
+++ src/Commands/Utilities/Help.js
@@ -0,0 +1,27 @@
+const Command = require('../../Structures/Command');
+const { MessageEmbed } = require('discord.js');
+
+module.exports = class extends Command {
+
+	constructor(...args) {
+		super(...args, {
+			aliases: ['help', 'halp']
+		});
+	}
+
+	async run(message, [command]) {
+		const embed = new MessageEmbed()
+			.setColor('BLUE')
+			.setAuthor(`${message.guild.name} Help Menu`, message.guild.iconURL({ dynamic: true }))
+			.setThumbnail(this.client.user.displayAvatarURL())
+			.setFooter(`Requested by ${message.author.username}`, message.author.displayAvatarURL({ dynamic: true }))
+			.setTimestamp();
+
+		if (command) {
+			const cmd = this.client.commands.get(command) || this.client.command.get(this.aliases.get(command));
+
+			if (!cmd) return message.channel.send(`\`${command}\` is not a valid command.`);
+		}
+	}
+
+};
