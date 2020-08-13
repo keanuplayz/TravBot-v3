10f4f30137e2aab52c2c8d7f40571d3f6858bf8f
Keanu <keanutimmermans8@gmail.com>
2020-08-13 22:03:18 +0200

    Added poll to fun.

--- src/commands/fun.ts
+++ src/commands/fun.ts
@@ -1,3 +1,4 @@
+import { MessageEmbed } from "discord.js";
 import Command from "../core/command";
 import {CommonLibrary} from "../core/lib";
 
@@ -41,6 +42,28 @@ export default new Command({
 				{
 					const sender = $.message.author;
 					$.channel.send(responses[Math.floor(Math.random() * responses.length)] + ` <@${sender.id}>`);
+				}
+			})
+		}),
+		poll: new Command({
+			description: "Create a poll.",
+			usage: "<question>",
+			run: "Please provide a question.",
+			any: new Command({
+				description: "Question for the poll.",
+				async run($: CommonLibrary): Promise<any>
+				{
+					const embed = new MessageEmbed()
+						.setAuthor(`Poll created by ${$.message.author.username}`, $.message.guild?.iconURL({ dynamic: true }) ?? undefined)
+						.setColor(0xffffff)
+						.setFooter("React to vote.")
+						.setDescription($.args.join(" "));
+					const msg = await $.channel.send(embed);
+					await msg.react("✅");
+					await msg.react("⛔");
+					$.message.delete({
+						timeout: 1000
+					});
 				}
 			})
 		})
