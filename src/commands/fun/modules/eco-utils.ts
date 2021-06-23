2cb94cc6ac0da603e4f45eda7d7c7989e73537ce
Keanu <keanutimmermans8@gmail.com>
2021-06-23 23:24:55 +0200

    Fix EL's PR.

--- src/commands/fun/modules/eco-utils.ts
+++ src/commands/fun/modules/eco-utils.ts
@@ -69,7 +69,7 @@ export function isAuthorized(guild: Guild | null, channel: TextChannel | DMChann
     if (guild?.id !== "637512823676600330") {
         channel.send("Sorry, this command can only be used in Monika's emote server.");
         return false;
-    } else if (channel?.id === "669464416420364288") {
+    } else if (channel?.id !== "669464416420364288") {
         channel.send("Sorry, this command can only be used in <#669464416420364288>.");
         return false;
     } else {
