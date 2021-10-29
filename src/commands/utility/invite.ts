203c541025acdd638c089a10c067112abe209f92
Alyxia Sother <lexisoth2005@gmail.com>
2021-10-29 15:27:52 +0200

    No more admin perms in the invite link!!!

--- src/commands/utility/invite.ts
+++ src/commands/utility/invite.ts
@@ -3,7 +3,11 @@ import {Command, NamedCommand} from "onion-lasers";
 export default new NamedCommand({
     description: "Gives you the invite link.",
     async run({send, client}) {
-        send(`https://discordapp.com/api/oauth2/authorize?client_id=${client.user!.id}&permissions=8&scope=bot`);
+        send(
+            `https://discordapp.com/api/oauth2/authorize?client_id=${
+                client.user!.id
+            }&permissions=138046467152&scope=bot`
+        );
     },
     number: new Command({
         async run({send, client, args}) {
