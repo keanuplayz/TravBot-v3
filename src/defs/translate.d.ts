69a845257458221e721be42bb099a35399b8d8d7
Alyxia Sother <lexisoth2005@gmail.com>
2021-11-03 13:52:39 +0100

    R.I.P. .translate, may you return another day

--- src/defs/translate.d.ts
+++
@@ -1,9 +0,0 @@
-interface TranslateOptions {
-    from?: string;
-    to?: string;
-}
-
-declare module "translate-google" {
-    function translate(input: string, options: TranslateOptions): Promise<string>;
-    export = translate;
-}
