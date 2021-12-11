1106cf7a4d25ec0fc155c57d4198b07288bfbea3
smartfridge <37928912+smartfrigde@users.noreply.github.com>
2021-12-11 18:31:10 +0100

    Move the rest of fun commands to interactions

--- src/commands/fun/figlet.ts
+++ src/commands/fun/figlet.ts
@@ -1,7 +1,24 @@
 import {NamedCommand, RestCommand} from "onion-lasers";
 import figlet from "figlet";
 import {Util} from "discord.js";
+import {SlashCommandBuilder} from "@discordjs/builders";
+import {CommandInteraction} from "discord.js";
 
+export const header = new SlashCommandBuilder()
+    .setDescription("Generates a figlet of your input.")
+    .addStringOption((option) =>
+        option.setName("text").setDescription("Text used to create the figlet.").setRequired(true)
+    );
+export async function handler(interaction: CommandInteraction) {
+    const {options} = interaction;
+    return interaction.reply(
+        `\`\`\`\n${Util.cleanCodeBlockContent(
+            figlet.textSync(options.getString("text", true), {
+                horizontalLayout: "full"
+            })
+        )}\n\`\`\``
+    );
+}
 export default new NamedCommand({
     description: "Generates a figlet of your input.",
     run: "You have to provide input for me to create a figlet!",
