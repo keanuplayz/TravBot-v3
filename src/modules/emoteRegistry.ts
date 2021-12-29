3170d85376cee300c24bb48af22980af957435e6
Alyxia Sother <lexisoth2005@gmail.com>
2021-12-29 20:17:39 +0000

    Fix a CRITICAL BUG in the emote dump module

--- src/modules/emoteRegistry.ts
+++ src/modules/emoteRegistry.ts
@@ -6,18 +6,20 @@ function updateGlobalEmoteRegistry(): void {
     const data: EmoteRegistryDump = {version: 1, list: []};
 
     for (const guild of client.guilds.cache.values()) {
-        for (const emote of guild.emojis.cache.values()) {
-            data.list.push({
-                ref: emote.name,
-                id: emote.id,
-                name: emote.name,
-                requires_colons: emote.requiresColons ?? false,
-                animated: emote.animated ?? false,
-                url: emote.url,
-                guild_id: emote.guild.name,
-                guild_name: emote.guild.name
-            });
-        }
+        guild.fetch().then((g) => {
+            for (const emote of g.emojis.cache.values()) {
+                data.list.push({
+                    ref: emote.name,
+                    id: emote.id,
+                    name: emote.name,
+                    requires_colons: emote.requiresColons ?? false,
+                    animated: emote.animated ?? false,
+                    url: emote.url,
+                    guild_id: emote.guild.name,
+                    guild_name: emote.guild.name
+                });
+            }
+        });
     }
 
     FileManager.open("data/public"); // generate folder if it doesn't exist
